6db9a25724736b82dc77ace2fb9839b62dd47974
Carlos <carlos.milkovi@gmail.com>
2022-09-23 11:06:22 +0100

    ...

---
+++ app1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace assignment1
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var options = OptionParser.GetInstance(args);
+            options.DisplayMeta();
+            new JsonTable().fromString(File.OpenText(options.Input).ReadToEnd());
+        }
+    }
+}
---
+++ app1/Table.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace assignment1
+{
+    public interface Table
+    {
+        Dictionary<string, Dictionary<int, string>> fromString(string s);
+        string toTable(Dictionary<string, Dictionary<int, string>> table);
+    }
+}
